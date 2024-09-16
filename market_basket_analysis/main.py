18d837c5e0461451deaabf253c996f8418c43b56
Mario Cerulo <96922764+MarioCerulo@users.noreply.github.com>
2024-09-16 10:05:09 +0200

    Comments

--- market_basket_analysis/main.py
+++ market_basket_analysis/main.py
@@ -33,14 +33,22 @@ def main():
     # Apriori algorithm
     frequent_itemsets = apriori(df, min_support=0.01, use_colnames=True)
 
+    # Add a column containing the lenght of the itemset
     frequent_itemsets["length"] = frequent_itemsets["itemsets"].apply(lambda x: len(x))
 
+    # Sort the itemset in descending order
     frequent_itemsets = frequent_itemsets.sort_values(by="support", ascending=False)
+
+    # Print the itemset containing 1 element with a minimum support of 2%
     print(frequent_itemsets[(frequent_itemsets["length"] == 1) & (frequent_itemsets["support"] >= 0.02)] [0:5])
 
+    # Find the association rules
     rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.01)
 
+    # Print the first 10 rules sorted by confidence
     print(rules.sort_values(by="confidence", ascending=False)[0:10])
+
+    # Print the rules with a minimum support of 1% and lift > 1
     print(rules[(rules["support"] >= 0.01) & (rules["lift"] > 1.0)])
 
 if __name__ == "__main__":
